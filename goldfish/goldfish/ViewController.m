c3ffd51a3b73a3e0e3c28473e6f95d9ee6deb549
Jesse Helton <jesse.helton@gmail.com>
2017-10-07 00:36:01 -0700

    - Added a GPUImageView to the View Controller - Added a recording camera - Attached a sample filter to the camera - Output the filter to the GPUImageView

--- goldfish/goldfish/ViewController.m
+++ goldfish/goldfish/ViewController.m
@@ -7,18 +7,41 @@
 //
 
 #import "ViewController.h"
+#import "GPUImage.h"
 
 @interface ViewController ()
+
+@property (nonatomic, strong) IBOutlet GPUImageView *cameraView;
+
+@property (nonatomic, strong) GPUImageVideoCamera *videoCamera;
+@property (nonatomic, strong) GPUImageFilter *customFilter;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+//NOTE: No need to call this function.  It is here to make sure these classes aren't stripped by the linker.
+- (void)keepAtLinkTime {
+    [GPUImageView class];
 }
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //Set up camera
+    _videoCamera = [[GPUImageVideoCamera alloc] initWithSessionPreset:AVCaptureSessionPreset640x480 cameraPosition:AVCaptureDevicePositionBack];
+    _videoCamera.outputImageOrientation = UIInterfaceOrientationPortrait;
+    
+    //Create image filter
+    _customFilter = [[GPUImageGrayscaleFilter alloc] init];
+    
+    //Hook up outputs to targets - camera->filter->view
+    [_videoCamera addTarget:_customFilter];
+    [_customFilter addTarget:self.cameraView];
+    
+    //Start the camera
+    [_videoCamera startCameraCapture];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
